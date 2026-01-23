0f0940b7d66ccd9a3fab3fb8dd3f2112e9e7c7ca
tongqiu <toqiu@amd.com>
2026-01-23 17:22:44 +0800

    Update triton_kernel_tutorial/gemm/simulate_group.py
    Co-authored-by: Xiake Sun <xisun@amd.com>

--- triton_kernel_tutorial/gemm/simulate_group.py
+++ triton_kernel_tutorial/gemm/simulate_group.py
@@ -74,7 +74,7 @@ if __name__ == "__main__":
     print("origin padding")
     simulate_pid_origin_mapping(M,N,K,BLOCK_SIZE_M,BLOCK_SIZE_N,BLOCK_SIZE_K)
 
-    print("gouped padding")
+    print("grouped padding")
     simulate_pid_mapping(M,N,K,BLOCK_SIZE_M,BLOCK_SIZE_N,BLOCK_SIZE_K,GROUP_SIZE_M)
     pass
 
